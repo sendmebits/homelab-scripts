2389402893d5626296cddfee2abb0b81051ee8e7
sendmebits <cgreco@gmail.com>
2024-12-14 20:15:21 -0500

    Update lxc-post-install.sh
    Add functions to clean up the code as I expect this will grow quite large and needs to be maintainable. More to come...

--- lxc/lxc-post-install.sh
+++ lxc/lxc-post-install.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
+# This is a post install script for Proxmox LXC's to automatically apply customizations after deployment
+# Author: sendmebits
+# License: MIT
+
+function header_info {
+clear
+cat <<"EOF"
+  _____          _     _____           _        _ _ 
+ |  __ \        | |   |_   _|         | |      | | |
+ | |__) |__  ___| |_    | |  _ __  ___| |_ __ _| | |
+ |  ___/ _ \/ __| __|   | | | '_ \/ __| __/ _` | | |
+ | |  | (_) \__ \ |_   _| |_| | | \__ \ || (_| | | |
+ |_|   \___/|___/\__| |_____|_| |_|___/\__\__,_|_|_|
+                                                    
+EOF
+}
+###############################################################
+#          Add shell "export LS_OPTIONS"
+###############################################################
+function shell_colour {
+# Define the .bashrc file path
+BASHRC_FILE="$HOME/.bashrc"
+
+# Check if the line exists and is commented
+if grep -q "^# export LS_OPTIONS='--color=auto'" "$BASHRC_FILE"; then
+    # Uncomment the line by removing the leading #
+    sed -i "s/^# export LS_OPTIONS='--color=auto'/export LS_OPTIONS='--color=auto'/" "$BASHRC_FILE"
+    echo "Uncommented the line in $BASHRC_FILE."
+else
+    echo "The line is either already uncommented or doesn't exist in $BASHRC_FILE."
+fi
+}
 
 ###############################################################
-#          Customize the shell to my liking
+#          Customize the shell ls to my liking
 ###############################################################
-
-#!/bin/bash
-
+function shell_ls_settings {
 # Path to .bashrc
 BASHRC="$HOME/.bashrc"
 
@@ -22,8 +52,8 @@ check_alias_exists() {
 
 # Check if both aliases already exist as expected
 if check_alias_exists "$ALIAS_LL" && check_alias_exists "$ALIAS_L"; then
-    echo "Both aliases already exist with the expected values. No changes needed."
-    exit 0
+    echo "Both ls aliases already exist with the expected values. No changes needed."
+    return 0
 fi
 
 # If we get here, at least one alias needs to be updated
@@ -38,16 +68,34 @@ sed -i.bak '/^alias l=/s/^/# /' "$BASHRC"
 
 # Add new aliases at the end of the file
 echo "" >> "$BASHRC"
-echo "# Added new aliases" >> "$BASHRC"
+echo "# Custom aliases" >> "$BASHRC"
 echo "$ALIAS_LL" >> "$BASHRC"
 echo "$ALIAS_L" >> "$BASHRC"
 
 echo "Aliases have been updated in $BASHRC"
 echo "A backup has been created at $BASHRC.backup"
 echo "Please run 'source ~/.bashrc' or start a new terminal session to apply changes"
+}
 
 ###############################################################
 #          Fix vim.tiny so arrow keys work
 ###############################################################
-echo "set nocompatible" > ~/.vimrc
-echo "vim.tiny arrow key fix applied"
+function vi_settings {
+VIM_SETTING="set nocompatible"
+VIMRC="$HOME/.vimrc"
+
+# Check if the line exists
+if grep -q "$VIM_SETTING" "$VIMRC/.vimrc" 2>/dev/null; then
+    # Uncomment the line by removing the leading #
+	echo $VIM_SETTING > $VIMRC/.vimrc
+	echo "vim.tiny arrow key fix applied"
+else
+    echo "vi already configured correctly, no updates needed."
+fi
+}
+
+# Run all the things
+header_info
+shell_colour
+shell_ls_settings
+vi_settings
